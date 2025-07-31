93af4cc05a09064b5457faa2184cb15b3b1b162b
kims <seemu.kim@fostermoore.com>
2025-08-01 07:59:37 +1200

    validate paging loading

--- src/main/java/com/datacom/qaexam/pages/QAPracticeMainPage.java
+++ src/main/java/com/datacom/qaexam/pages/QAPracticeMainPage.java
@@ -2,6 +2,7 @@ package com.datacom.qaexam.pages;
 
 import com.datacom.qaexam.configloader.AppConfig;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.LoadState;
 
 public class QAPracticeMainPage extends BasePage{
     public QAPracticeMainPage(Page page) {
@@ -10,6 +11,7 @@ public class QAPracticeMainPage extends BasePage{
 
     public void openPage() {
         page.navigate(AppConfig.getInstance().getHomePageUrl());
+        page.waitForLoadState(LoadState.LOAD); // wait until the main page is fully loaded
     }
 
     public SpotBugsPage navigateToSpotTheBugsChallenge() {
