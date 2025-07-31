7e028c6b0a0166f9b892f490f5f3c06c4de64b48
kims <seemu.kim@fostermoore.com>
2025-08-01 07:51:12 +1200

    to run scenarios in parallel via mvn clean test. This will be used in CICD

--- src/test/java/com/datacom/qaexam/runners/RunCucumberTest.java
+++ src/test/java/com/datacom/qaexam/runners/RunCucumberTest.java
@@ -5,6 +5,7 @@ import org.junit.platform.suite.api.*;
 @IncludeEngines("cucumber")
 @SelectPackages("features") // run all feature files under src/test/resources/features
 @ConfigurationParameter(key = "cucumber.glue", value = "com.datacom.qaexam") // search for step definitions and hooks under com.datacom.qaexam
+@ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "true") // To run cucumber scenarios in parallel via mvn clean test
 public class RunCucumberTest {
 
 
